8edae53137c6d8ba672cd4ddeeef243461779fed
rafael.possenti <rafael41051010>
2021-07-26 18:25:50 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/possenti/social_net/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.possenti.social_net
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/detalhes/detalhes.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:social_net/modelos/receita.dart';
+
+class Detalhes extends StatelessWidget {
+  final Receita? receita;
+
+  Detalhes({Key? key, @required this.receita}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _construirDetalhes();
+  }
+
+  Widget _construirDetalhes() {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          _construirImagemDetalhes(receita!.foto!),
+          // _construirTituloDetalhes(receita!.titulo!),
+
+          _construirLinhaIconesDetalhes(
+              receita!.tempoPreparo!, '${receita!.porcoes!} porções'),
+
+          _construirTextoDetalhes(receita!.ingredientes!),
+
+          // _construirSubtitulosDetalhes('Ingredientes'),
+
+          // _construirSubtitulosDetalhes('Modo de preparo'),
+          // _construirTextoDetalhes(receita!.modoPreparo!)
+        ],
+      ),
+      appBar: _construirAppBar('Rede Social'),
+    );
+  }
+
+  Widget _construirImagemDetalhes(String imagem) {
+    return Image.asset(imagem);
+  }
+
+  Widget _construirTituloDetalhes(String titulo) {
+    return Center(
+      child: Text(titulo,
+          style: TextStyle(color: Colors.deepOrange, fontSize: 30)),
+    );
+  }
+
+  Widget _construirSubtitulosDetalhes(String subtitulo) {
+    return Center(child: Text(subtitulo, style: TextStyle(fontSize: 20)));
+  }
+
+  Widget _construirTextoDetalhes(String texto) {
+    return Container(padding: EdgeInsets.all(20), child: Text(texto));
+  }
+
+  AppBar _construirAppBar(String titulo) {
+    return AppBar(title: Text(titulo));
+  }
+
+  Widget _construirLinhaIconesDetalhes(String tempo, String rendimento) {
+    return Row(
+        children: <Widget>[Icon(Icons.thumb_up, color: Colors.deepOrange)]);
+  }
+}
